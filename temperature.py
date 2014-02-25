a13dd795a5fd36e156359c7dbf85231b268aef11
Ystrem <ystrem@gmail.com>
2014-02-25 18:13:07 +0100

    Some changes.

--- temperature.py
+++ temperature.py
@@ -2,12 +2,12 @@
 import os
 import glob
 import time
-import sqllite3 as lite
+import sqlite3 as lite
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-thermometers = ['28-0000035ac85e', '28-0000035ac8a7', '28-0000035acb40']
+thermometers = ['28-0000035ac85e','28-0000035acb40']
 temperatures = []
 base_dir = '/sys/bus/w1/devices/'
 device_file = '/w1_slave'
@@ -15,16 +15,25 @@ device_file = '/w1_slave'
 def saveToDB(data):
 	con = lite.connect('temp.db')
 	
-	with con :
+	with con:
 		cur = con.cursor()
 		
-		cur.execute("CREATE TABLE IF NOT EXISTS Temperatures(Date TEXT, Inside TEXT, Outside TEXT)")
-		cur.execute("INSERT INTO Temperatures(Date, Inside, Outside) VALUES(?, ?, ?)", data)
+		cur.execute("CREATE TABLE IF NOT EXISTS Temperatures(Date DATE, Inside TEXT, Outside TEXT)")
+		cur.execute("INSERT INTO Temperatures(Date, Inside, Outside) VALUES(datetime('now','localtime'), ?, ?)", data)
 		
 		con.commit()
 
+def readFromDB():
+	temp = []
+	con = lite.connect('temp.db')
+	
+	with con:
+		cur = con.cursor()
+		
+		cur.execute("SELECT ")
+
 def temperature():
-	temperatures.append(str(time.strftime('%Y-%m-%d %H:%M:%S ,', time.localtime())))
+	#temperatures.append(str(time.strftime('%Y-%m-%d %H:%M:%S ,', time.localtime())))
 	for thermometer in thermometers:
 		with open(base_dir + thermometer + device_file,'r') as f:
 			lines = f.readlines()
@@ -37,6 +46,8 @@ def temperature():
 			print 'File is closed = ', f.closed
 		
 	return temperatures
+
+
 		
 if __name__ == '__main__':
 	saveToDB(temperature())
