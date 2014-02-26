0f911bd8425190c6e77bdeeee36dc44b05516af3
Ystrem <ystrem@gmail.com>
2014-02-26 21:58:54 +0100

    Added ReadFromDB.

--- temperature.py
+++ temperature.py
@@ -11,9 +11,10 @@ thermometers = ['28-0000035ac85e','28-0000035acb40']
 temperatures = []
 base_dir = '/sys/bus/w1/devices/'
 device_file = '/w1_slave'
+DB_NAME = 't.db'
 
 def saveToDB(data):
-	con = lite.connect('temp.db')
+	con = lite.connect(DB_NAME)
 	
 	with con:
 		cur = con.cursor()
@@ -25,12 +26,22 @@ def saveToDB(data):
 
 def readFromDB():
 	temp = []
-	con = lite.connect('temp.db')
+	con = lite.connect(DB_NAME)
 	
 	with con:
 		cur = con.cursor()
 		
-		cur.execute("SELECT ")
+		week = cur.execute("SELECT * FROM Temperatures ORDER BY DATE LIMIT 25200")
+		day = cur.execute("SELECT * FROM Temperatures ORDER BY DATE LIMIT 3600")
+		
+	print day
+	
+	with open('t.log','w') as f:
+
+		for item in day:
+			f.writelines(item)
+			f.writelines('\n')
+			print item
 
 def temperature():
 	#temperatures.append(str(time.strftime('%Y-%m-%d %H:%M:%S ,', time.localtime())))
@@ -51,4 +62,4 @@ def temperature():
 		
 if __name__ == '__main__':
 	saveToDB(temperature())
-	
+	#readFromDB()	
